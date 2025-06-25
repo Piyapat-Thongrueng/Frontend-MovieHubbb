b5590686b6b88cea5bcfe31bd55ec6ca13ddd5b8
Piyapat <piyapat.thongrueng@gmail.com>
2025-06-25 16:37:47 +0700

    Complete Navbar and Integrate Clerk Authentication for users.

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg'
+import logo from './popcorn.svg'
 import marvelLogo from './marvelLogo.svg'
 import googlePlay from './googlePlay.svg'
 import appStore from './appStore.svg'
